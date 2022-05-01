8370e141af82318b806041da1d2ca3584922e56f
fabianafernandes2002 <fabianafernandes2002@hotmail.com>
2022-05-01 07:46:00 -0300

    react1

---
+++ public/src/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import reactDom from "react-dom"
+import "./styles.css"
+
+const element = 'Digital Inovation'
+const element2 = <h1>Olá Turma =D</h1>
+
+
+const rootElement = document.getElementById("root")
+ReactDOM.render(element, rootElement)
